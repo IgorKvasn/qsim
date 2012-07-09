34882e14fdf9066d8e37ad85851ea317d28b33ef
igor.kvasnicka@gmail.com <igor.kvasnicka@gmail.com@27e0d09e-6ca6-0940-9b04-fac040e3cc4e>
2012-07-09 10:28:16 +0000

    listeners for adding/removing simulation or ping rules

---
+++ trunk/Simulation/src/main/java/sk/stuba/fiit/kvasnicka/qsimsimulation/events/pingrule/PingRuleEvent.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * This file is part of qSim.
+ *
+ * qSim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qSim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qSim.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+/*******************************************************************************
+ * This file is part of qSim.
+ *
+ * qSim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qSim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qSim.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package sk.stuba.fiit.kvasnicka.qsimsimulation.events.pingrule;
+
+import lombok.Getter;
+import sk.stuba.fiit.kvasnicka.qsimsimulation.rule.SimulationRuleBean;
+
+import java.util.EventObject;
+
+/**
+ * @author Igor Kvasnicka
+ */
+public class PingRuleEvent extends EventObject {
+    @Getter
+    private SimulationRuleBean rule;
+
+    public PingRuleEvent(Object source, SimulationRuleBean rule) {
+        super(source);
+        this.rule = rule;
+    }
+}
---
+++ trunk/Simulation/src/test/java/sk/stuba/fiit/kvasnicka/qsimsimulation/managers/PingManagerTest.java
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * This file is part of qSim.
+ *
+ * qSim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qSim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qSim.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package sk.stuba.fiit.kvasnicka.qsimsimulation.managers;
+
+import org.junit.Before;
+import org.junit.Test;
+import sk.stuba.fiit.kvasnicka.qsimdatamodel.data.NetworkNode;
+import sk.stuba.fiit.kvasnicka.qsimdatamodel.data.Router;
+import sk.stuba.fiit.kvasnicka.qsimsimulation.enums.Layer4TypeEnum;
+import sk.stuba.fiit.kvasnicka.qsimsimulation.enums.PacketTypeEnum;
+import sk.stuba.fiit.kvasnicka.qsimsimulation.rule.SimulationRuleBean;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * @author Igor Kvasnicka
+ */
+public class PingManagerTest {
+    private PingManager pingManager;
+    private NetworkNode node1, node2;
+
+
+    @Before
+    public void before() {
+        pingManager = new PingManager();
+
+        node1 = new Router("node1", null, null, 10, 10, 10, 10, 100, 0, 0);
+        node2 = new Router("node2", null, null, 10, 10, 10, 10, 100, 0, 0);
+    }
+
+    @Test
+    public void testPingAdd() {
+        SimulationRuleBean rule = new SimulationRuleBean(node1, node2, 0, 0, 0, PacketTypeEnum.AUDIO_PACKET, Layer4TypeEnum.UDP, true);
+        setWithoutSetter(SimulationRuleBean.class, rule, "route", Arrays.asList(node1, node2));
+        pingManager.addPing(rule, 1);
+
+        assertNotNull(getPropertyWithoutGetter(PingManager.class, pingManager, "pingDefinitions"));
+        Map pingDefinitions = (Map) getPropertyWithoutGetter(PingManager.class, pingManager, "pingDefinitions");
+        assertEquals(1, pingDefinitions.size());
+
+        assertNotNull(pingManager.getPingSimulationRules());
+        assertEquals(1, pingManager.getPingSimulationRules().size());
+    }
+
+    @Test
+    public void testPingRemove() {
+
+        //preparation
+        testPingAdd(); //first add some ping simulation rule
+        SimulationRuleBean rule = pingManager.getPingSimulationRules().get(0);//there cannot be any NullPointerEx, because it was checked in the method above
+
+        //test
+        pingManager.removePing(rule);//remove the rul
+
+        //assert
+        assertNotNull(getPropertyWithoutGetter(PingManager.class, pingManager, "pingDefinitions"));
+        Map pingDefinitions = (Map) getPropertyWithoutGetter(PingManager.class, pingManager, "pingDefinitions");
+        assertEquals(0, pingDefinitions.size());
+
+        assertNotNull(pingManager.getPingSimulationRules());
+        assertEquals(0, pingManager.getPingSimulationRules().size());
+    }
+
+
+    private Object getPropertyWithoutGetter(Class klass, Object bean, String field) {
+        Field f = null;
+        try {
+            f = klass.getDeclaredField(field);
+            f.setAccessible(true);
+            return f.get(bean);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void setWithoutSetter(Class c, Object o, String field, Object value) {
+
+        Field f = null;
+        try {
+            f = c.getDeclaredField(field);
+            f.setAccessible(true);
+            f.set(o, value);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
